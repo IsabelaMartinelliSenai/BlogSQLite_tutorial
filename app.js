e5dd2b4b210ec7250306cf0ede56cd71c2e9f25a
IsabelaMartinelliSenai <isabelamartinelli2007@gmail.com>
2025-03-14 13:27:08 -0300

    rotas do servidor

--- app.js
+++ app.js
@@ -6,12 +6,25 @@ const PORT = 3000; //porta TCP do servidor HTTP da aplicação
 const app = express();
 //cria uma instância da classe da biblioteca "express"
 
+const index = "<a href='/sobre'>Sobre</a> <a href='/info'>Info</a>";
+
 /*Método expres.get necessita de dois parâmetros
 Na ARROW FUNCTION, o primeiro são os dados do servidor (REQUISITION - 'req')
 o segundo, são os dados que serão enviados ao cliente (RESULT - 'res)
 */
 app.get("/", (req, res) => {
-  res.send("Olá SESI");
+  //rota raiz do servidor, acesse o browser com o endereço http://localhost:3000/
+  res.send(index);
+});
+
+app.get("/sobre", (req, res) => {
+  //rota raiz do servidor, acesse o browser com o endereço http://localhost:3000/sobre
+  res.send("Você está na página Sobre");
+});
+
+app.get("/info", (req, res) => {
+  //rota raiz do servidor, acesse o browser com o endereço http://localhost:3000/info
+  res.send("Você está na página Info");
 });
 
 //app.listen() deve ser o último comando da aplicação
